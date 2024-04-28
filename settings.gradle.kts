0ad7fc8e3a09d1634a03c6c02d9621515639d738
Novalf <novalfajarbudipamungkas@gmail.com>
2024-04-28 12:07:47 +0700

    Tugas TipTime

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tip_Time22"
+include(":app")
+ 
